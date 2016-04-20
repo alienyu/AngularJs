be82ce8a8c23c4fee326b830cd4d1fb8bb7c2119
alienyu <alienyu>
2016-04-21 01:15:32 +0800

    fh

--- Start/static/js/logic/app.js
+++ Start/static/js/logic/app.js
@@ -1,9 +1,17 @@
-function MyController($scope, $timeout) {
-	var updateLock = function() {
-		$scope.clock = new Date();
-		$timeout(function() {
-			updateLock();
-		},1000);
-	};
-	updateLock();
-};
+var myModule = angular.module("HelloAngular", []);
+
+myModule.controller("helloAngular", ['$scope',
+	function HelloAngular($scope) {
+		$scope.greeting = {
+			text: "Hello"
+		}
+	}
+]); 
+
+myModule.directive("hello", function() {
+	return {
+		restrict: 'E',
+		template: '<div>Hi everyone!</div>',
+		replace: true
+	}
+});
